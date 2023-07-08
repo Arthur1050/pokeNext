eb77528b4c27a19c55064c07d2ff04d6048decf9
Arthur Tosta <tostamatias@gmail.com>
2023-07-08 20:19:37 -0300

    wip: side menu

--- components/organisms/SideMenu/SideMenu.tsx
+++ components/organisms/SideMenu/SideMenu.tsx
@@ -1,11 +1,36 @@
+import { PokeBall } from '@/public/assets/svg/Pokeball'
 import * as Styles from './Styles'
+import { Info } from 'lucide-react'
+import { useRef, useState } from 'react'
+
+export const itemsMenu = [
+    {
+        icon: (<PokeBall />),
+        name: 'Pokemons'
+    },
+    {
+        icon: (<Info size={21}/>),
+        name: 'Sobre'
+    }
+]
 
 export default function SideMenu() {
+    const [selState, setSelState] = useState({})
+    const firstItem = useRef()
+    const handleMenuItems = () => {
+
+    }
     return(
-        <>
-            <Styles.SideMenu>
-                
-            </Styles.SideMenu>
-        </>
+        <Styles.SideMenu>
+            <ul>
+                <div style={{height: '32px', top: '16px'}}/>
+                {itemsMenu.map((el, i) => (
+                    <li ref={!i && firstItem}>
+                        {el.icon}
+                        <span>{el.name}</span>
+                    </li>
+                ))}
+            </ul>
+        </Styles.SideMenu>
     )
 }
--- components/organisms/SideMenu/Styles.tsx
+++ components/organisms/SideMenu/Styles.tsx
@@ -1,5 +1,29 @@
 import { styled } from "styled-components";
 
 export const SideMenu = styled.aside`
-    background-color: #000;
+    padding: .5rem 0;
+    border-radius: .5rem;
+    background-color: #101314;
+    position: relative;
+    li {
+        position: relative;
+        display: flex;
+        align-items: center;
+        padding: .375rem .5rem;
+        gap: .5rem;
+        color: #fff;
+        cursor: pointer;
+        transition: .2s backdrop-filter;
+        &:hover {
+            backdrop-filter: brightness(1.4);
+        }
+    }
+    div {
+        background-color: #DE1537;
+        position: absolute;
+        width: 100%;
+    }
+    hr {
+        opacity: .05;
+    }
 `
