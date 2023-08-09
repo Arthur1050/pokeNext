f3faf2235ca233e5d7e955eb219e7f66ae47019f
Arthur Tosta <tostamatias@gmail.com>
2023-08-08 21:00:22 -0300

    wip

--- components/molecules/MountCards/MountCards.tsx
+++ components/molecules/MountCards/MountCards.tsx
@@ -6,7 +6,7 @@ import { Suspense, use, useEffect, useRef, useState } from "react"
 import { SkeletonDiv } from "@/components/atoms/SkeletonDiv/SkeletonDiv"
 import { GridPokemonsStyle } from "@/components/organisms/GridPokemons/styles"
 
-export default function MountCards({
+export default async function MountCards({
     pokemons,
     getPokemons,
     getData
@@ -16,31 +16,55 @@ export default function MountCards({
     getData: (selectedId:string) => Promise<{}>
 }) {
     const loadMoreMsg = useRef<HTMLDivElement>(null)
-    const [arrayPokemons, setArrayPokemons] = useState(use(pokemons).results)
+    const [arrayPokemonsSection, setArrayPokemonsSection] = useState([<></>])
 
-    const observer = (typeof window !== "undefined") ? new IntersectionObserver(entries => {
+    const loadInitialPokemons = async () => {
+        setArrayPokemonsSection([
+            <>
+                {(await pokemons).results.map((pokemon) => (
+                    <CardPokemon key={pokemon.id} pokemon={pokemon} getData={getData}/>
+                ))}
+            </>
+        ])
+    }
+
+    useEffect(() => {
+        console.log(`renderizou`)
+        loadInitialPokemons()
+        loadMoreMsg.current && observer?.observe(loadMoreMsg.current)
+    }, [])
+
+    const observer = (typeof window !== "undefined") ? new IntersectionObserver(async entries => {
         const [entry] = entries;
         console.log(entry.isIntersecting);
-        entry.isIntersecting && setArrayPokemons(currentArray => 
+
+        if (entry.isIntersecting) {
+            let morePokemons = await getPokemons(30, arrayPokemonsSection.length*30)
+            arrayPokemonsSection.push(
+                <>
+                    {morePokemons.results.map((pokemon) => (
+                        <CardPokemon key={pokemon.id} pokemon={pokemon} getData={getData}/>
+                    ))}
+                </>
+            );
+            setArrayPokemonsSection(arrayPokemonsSection);
+        }
+        /* entry.isIntersecting && setArrayPokemons(currentArray => 
             currentArray.concat(use(getPokemons(30, currentArray.length)).results)
-        );
+        ); */
     }, {
         threshold: 1.0,
         root: null
     }):null;
 
-    useEffect(() => {
-        console.log(`renderizou`)
-
-        loadMoreMsg.current && observer?.observe(loadMoreMsg.current)
-    }, [])
     return (
         <>
-            <Suspense fallback={<SKDGridPokemon />}>
-                {arrayPokemons.map((pokemon) => (
-                    <CardPokemon key={pokemon.id} pokemon={pokemon} getData={getData}/>
-                ))}
-            </Suspense>
+            {arrayPokemonsSection.map(pokemonSection => (
+                    <Suspense fallback={<SKDGridPokemon />}>
+                        {pokemonSection}
+                    </Suspense>
+                )
+            )}
             <div ref={loadMoreMsg} className="w-full flex items-center justify-center gap-2 my-3 select-none">
                 <p>Carregando mais...</p>
                 <div className="animate-spin">
