a7ff3acb5424c4a6a78f6fdeeeadd9a1b39362e9
Arthur Tosta <tostamatias@gmail.com>
2023-08-20 02:54:29 -0300

    feat: Finalizado parte de loadmore

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Overlay from '@/components/atoms/overlay/Overlay'
 import GridPokemons from '@/components/organisms/GridPokemons/GridPokemons'
 import ModalPokeDetails from '@/components/organisms/ModalPokeDetails/ModalPokeDetails'
+import { getDataPokemon } from "@/app/actions";
 import { Suspense } from 'react'
 
 export default function Home() {
@@ -8,7 +9,7 @@ export default function Home() {
   return (
     <>
       <main className='bg-slate-100 rounded-lg overflow-auto'>
-        <GridPokemons />
+        <GridPokemons getData={getDataPokemon}/>
       </main>
       <Overlay>
         <ModalPokeDetails/>
--- components/organisms/GridPokemons/GridPokemons.tsx
+++ components/organisms/GridPokemons/GridPokemons.tsx
@@ -1,7 +1,9 @@
-import styles from './GridPokemon.module.css'
+'use client'
+import { Suspense, useCallback, useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react';
 import { GridPokemonsStyle } from "./styles";
-import { getDataPokemon } from "@/app/actions";
-import MountCards from "@/components/molecules/MountCards/MountCards";
+import { PokeballFill } from '@/public/assets/svg/pokeballFill';
+import { SkeletonDiv } from '@/components/atoms/SkeletonDiv/SkeletonDiv';
+import CardPokemon from '@/components/molecules/CardPokemon';
 
 export interface Pokemons {
     name: string,
@@ -9,7 +11,6 @@ export interface Pokemons {
 }
 
 async function getPokemons(limit:number, offset:number):Promise<{results: Pokemons[]}> {
-    "use server"
     const api = 'https://pokeapi.co/api/v2/pokemon/';
 
     const res = await fetch(`${api}?limit=${limit}&offset=${offset}`, {
@@ -19,18 +20,85 @@ async function getPokemons(limit:number, offset:number):Promise<{results: Pokemo
     const data:Promise<{results: Pokemons[]}> = res.json();
 
     (await data).results.forEach((pokemon, i) => {
-        pokemon.id = i + 1;
+        pokemon.id = i + 1 + offset;
     })
 
     return data;
 }
 
-export default function GridPokemons() {
-    const pokemons = getPokemons(30, 0);
+export default function GridPokemons({getData}:{
+    getData: (selectedId:string) => Promise<{}>
+}) {
+    const loadMoreMsg = useRef<HTMLDivElement>(null)
+    const gridContainer = useRef<HTMLDivElement>(null)
+    const [pokemonsBlock, setPokemonsBlock] = useState<Array<JSX.Element[]>>([])
+
+    async function loadPokemons() {
+        const currentPokemonsCount = gridContainer.current ? gridContainer.current.childElementCount-1 : 0;
+        const data = await getPokemons(30, currentPokemonsCount);
+         
+        // Monta um bloco de pokemons
+        const blockResult = data.results.map(pokemon => <CardPokemon pokemon={pokemon} getData={getData}/>)
+
+        setPokemonsBlock(prevPokemonsBlock => [...prevPokemonsBlock , blockResult])
+    }
+
+    const observerFunction = (entries:IntersectionObserverEntry[]) => {
+        const [entry] = entries;
+        console.log(entry.isIntersecting);
+
+        if (entry.isIntersecting) {
+            loadPokemons()
+        }
+    }
+
+    useEffect(() => {
+        console.log(`renderizou`)
+        const observer = (typeof window !== "undefined") ? new IntersectionObserver(observerFunction, {
+            threshold: 1.0,
+            root: null
+        }):null;
+    
+        loadMoreMsg.current && observer?.observe(loadMoreMsg.current)
+        
+        return () => {
+            observer?.disconnect()
+        }
+    }, [])
+
+    useLayoutEffect(() => {
+    }, [])
 
     return(
-        <GridPokemonsStyle>
-            <MountCards getData={getDataPokemon} pokemons={pokemons} getPokemons={getPokemons}/>
+        <GridPokemonsStyle ref={gridContainer}>
+            <>
+                {pokemonsBlock.map(block => (
+                        <Suspense fallback={<SKDGridPokemon />}>
+                            {block}
+                        </Suspense>
+                    )
+                )}
+                <div ref={loadMoreMsg} className="w-full flex items-center justify-center gap-2 my-3 select-none">
+                    <p>Carregando mais...</p>
+                    <div onClick={() => loadPokemons()} className="animate-spin">
+                        <PokeballFill />
+                    </div>
+                </div>
+            </>
         </GridPokemonsStyle>
     )
 }
+
+function SKDGridPokemon() {
+    const cards = [];
+    for(let i = 0; i <= 30; i++) {
+      cards.push(
+        <SkeletonDiv className='rounded-lg bg-red-700 w-[8rem] h-[11rem] flex-grow'/>
+      )
+    }
+    return(
+      <GridPokemonsStyle className=''>
+        {cards}
+      </GridPokemonsStyle>
+    )
+  }
